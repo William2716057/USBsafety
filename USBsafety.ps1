41a76c300f7eda55a1052203487a2ebdd1fda119
William <willspri@gmail.com>
2025-03-14 12:33:33 +1300

    updated

--- USBsafety.ps1
+++ USBsafety.ps1
@@ -5,6 +5,10 @@ $usbDevices = Get-CimInstance Win32_DiskDrive | Where-Object { $_.MediaType -eq 
 
 if ($usbDevices) {
         Write-Host "USB detected"
+	$usbDevices | forEach-Object { Write-Host " - $($_.DeviceID) ($($_.Model))" }
 } else {
         Write-Host "Waiting"
+	Start-Sleep -Seconds 5
 }
+
+#Start-Sleep -Seconds 20
